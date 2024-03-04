ed8eb1b58d95ac79bbee09101577c7517867bd1a
seasons986 <annalin12345@gmail.com>
2024-03-03 21:54:36 -0500

    let's get poppin

--- index.js
+++ index.js
@@ -10,36 +10,43 @@ window.addEventListener("load", () => {
   let int;
   let card = 0;
   const balloons = [];
+  const popped = [];
   const colors = ["green", "orange", "purple", "red"];
-  const types = ["Balloon", "Pop"];
-  colors.forEach((color) => balloons.push(`assets/${color}Balloon.gif`));
-  console.log(balloons);
+  colors.forEach((color) => {
+    balloons.push(`assets/${color}Balloon.gif`);
+    popped.push(`assets/${color}Pop.gif`);
+  });
+  console.log(balloons, popped);
 
   function drawCard() {
-    if (card > 5) return clearInterval(int);
-
+    if (card > getRandomInt(25,50)) return clearInterval(int);
+    const rand = getRandomInt(0,balloons.length);
+    const balloon = `https://iguannalin.github.io/balloons/${balloons[rand]}`;
+    const pop = `https://iguannalin.github.io/balloons/${popped[rand]}`;
     const text = `<!DOCTYPE html><html> <head> <title>${card+1}</title><meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
       body{margin:0 auto;text-align:center;color: #f9faffde;
       text-shadow: #858ebc -2px 2px 8px;width:100vw;height:100vh;
       display:flex;align-items:center;justify-content:center;background-color:rgb(35, 35, 35);}
-      img{width:60%;height:auto;}
+      img{width:60%;height:auto;cursor:pointer;}
     </style>
     <body>
       <div>
-          <img src="https://iguannalin.github.io/balloons/${balloons[getRandomInt(0,balloons.length)]}"/>
+          <img id="balloon" src="${balloon}"/>
       </div>
       <script>
-      let x = (Math.random()*window.screenLeft)+(window.screenLeft-100);
-      let y = window.screenTop;
-      window.setInterval(()=>{
+        const balloon = document.getElementById("balloon");
+        let x = (Math.random()*window.screenLeft)+(window.screenLeft-100);
+        let y = window.screenTop;
+        window.setInterval(()=>{
           y+=1;
           x=Math.random()>0.5?x+1:x;
-          window.moveTo(x, y);},50);
-          window.onclick = () => alert("clicked!");
-      </script>
-      </body></html>`;
-    const blob = new Blob([text], {type: "text/html"});
+          window.moveTo(x, y);
+        },50);
+        window.onclick = () => {balloon.src="${pop}";setTimeout(window.close, 250)};
+        </script>
+        </body></html>`;
+        const blob = new Blob([text], {type: "text/html"});
     const blobUrl = URL.createObjectURL(blob);
     window.open(blobUrl, '_blank', `popup,location,status,scrollbars,resizable,width=100,height=100,top=0,left=${getRandomInt(0,window.innerWidth)}`);
     window.URL.revokeObjectURL(blobUrl);
