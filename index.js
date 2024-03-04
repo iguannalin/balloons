39124213a8f7c2fd16a3a32a5a144b4f569a8c2a
seasons986 <annalin12345@gmail.com>
2024-03-04 00:22:06 -0500

    Heehee

--- index.js
+++ index.js
@@ -12,14 +12,29 @@ window.addEventListener("load", () => {
   const balloons = [];
   const popped = [];
   const colors = ["green", "orange", "purple", "red"];
+  // assets created by www.2nd.systems!!!
   colors.forEach((color) => {
     balloons.push(`assets/${color}Balloon.gif`);
     popped.push(`assets/${color}Pop.gif`);
   });
-  console.log(balloons, popped);
+  // viridis color palette from--https://waldyrious.net/viridis-palette-generator/
+  const bgs = [
+      "#f0f921",
+      "#fcd225",
+      "#fdae32",
+      "#f68d45",
+      "#e76f5a",
+      "#d5546e",
+      "#c03a83",
+      "#a62098",
+      "#8606a6",
+      "#6300a7",
+      "#3e049c",
+      "#0d0887"
+  ];
 
   function drawCard() {
-    if (Math.random() > 0.7) return;
+    if (Math.random() > 0.6) return; // manually staggering balloons
     if (card > getRandomInt(25,70)) return clearInterval(int);
     const rand = getRandomInt(0,balloons.length);
     const balloon = `https://iguannalin.github.io/balloons/${balloons[rand]}`;
@@ -36,23 +51,34 @@ window.addEventListener("load", () => {
           <img id="balloon" src="${balloon}"/>
       </div>
       <script>
+        document.body.style.backgroundColor = "${bgs[getRandomInt(0,bgs.length)]}";
         const balloon = document.getElementById("balloon");
         let x = window.screenLeft;
         let y = 0;
         window.setInterval(()=>{
+          if (y >= screen.height-200) {
+            balloon.src="${pop}";
+            setTimeout(window.close, 300);
+          }
           y+=1;
-          x=Math.random()>0.3?x+1:Math.random()>0.3?x-1:x;
+          x=Math.random()>0.3?x+0.5:Math.random()>0.3?x-0.5:x;
           window.moveTo(x, y);
         },25);
-        window.onclick = () => {balloon.src="${pop}";setTimeout(window.close, 250)};
+        window.onclick = () => {
+          balloon.src="${pop}";
+          setTimeout(window.close, 250);
+        };
         </script>
         </body></html>`;
         const blob = new Blob([text], {type: "text/html"});
     const blobUrl = URL.createObjectURL(blob);
     window.open(blobUrl, '_blank', `popup,location,status,scrollbars,resizable,width=100,height=100,top=0,left=${getRandomInt(200,screen.width-200)}`);
     window.URL.revokeObjectURL(blobUrl);
+    document.body.style.backgroundColor=bgs[getRandomInt(0,bgs.length)];
     card++;
   }
 
-  button.onclick = () => {int = setInterval(drawCard, 500);};
+  button.onclick = () => {
+    int = setInterval(drawCard, 500);
+  };
 });
