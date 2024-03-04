fee7a7a1d5a8e7a07449355a09d2cb94bef4dd76
seasons986 <annalin12345@gmail.com>
2024-03-03 21:45:26 -0500

    testing

--- index.js
+++ index.js
@@ -9,27 +9,42 @@ window.addEventListener("load", () => {
   const button = document.getElementById("button");
   let int;
   let card = 0;
+  const balloons = [];
   const colors = ["green", "orange", "purple", "red"];
-  const types = ["Balloon", "Pop"]
+  const types = ["Balloon", "Pop"];
+  colors.forEach((color) => balloons.push(`assets/${color}Balloon.gif`));
+  console.log(balloons);
 
-  let angle = 0;
-  let step = (2*Math.PI) / 12;
   function drawCard() {
-    if (card > 11) return clearInterval(int);
-    let radius = 300;
-    let x = 500 + radius * Math.cos(angle);
-    let y = 375 + radius * Math.sin(angle);
-    angle += step;
-    console.log({card,x,y});
+    if (card > 5) return clearInterval(int);
 
-    const text = `<!DOCTYPE html><html> <head> <title>${card+1}</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"><style>body{margin:0 auto;text-align:center;color: #f9faffde;
-      text-shadow: #858ebc -2px 2px 8px;width:100vw;height:100vh;background-color:rgb(35, 35, 35);}</style><div><marquee>${card}</marquee></div><script>let x = window.screenLeft; let y = window.screenTop; window.setInterval(()=>{x+=1;window.moveTo(x, y);},50);</script></body></html>`;
+    const text = `<!DOCTYPE html><html> <head> <title>${card+1}</title><meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      body{margin:0 auto;text-align:center;color: #f9faffde;
+      text-shadow: #858ebc -2px 2px 8px;width:100vw;height:100vh;
+      display:flex;align-items:center;justify-content:center;background-color:rgb(35, 35, 35);}
+      img{width:60%;height:auto;}
+    </style>
+    <body>
+      <div>
+          <img src="https://iguannalin.github.io/balloons/${balloons[getRandomInt(0,balloons.length)]}"/>
+      </div>
+      <script>
+      let x = (Math.random()*window.screenLeft)+(window.screenLeft-100);
+      let y = window.screenTop;
+      window.setInterval(()=>{
+          y+=1;
+          x=Math.random()>0.5?x+1:x;
+          window.moveTo(x, y);},50);
+          window.onclick = () => alert("clicked!");
+      </script>
+      </body></html>`;
     const blob = new Blob([text], {type: "text/html"});
     const blobUrl = URL.createObjectURL(blob);
-    window.open(blobUrl, '_blank', `popup,location,status,scrollbars,resizable,width=100,height=100,top=${getRandomInt(250,400)},left=${x}`);
+    window.open(blobUrl, '_blank', `popup,location,status,scrollbars,resizable,width=100,height=100,top=0,left=${getRandomInt(0,window.innerWidth)}`);
     window.URL.revokeObjectURL(blobUrl);
     card++;
   }
 
-  button.onclick = () => {int = setInterval(drawCard, 200);};
+  button.onclick = () => {int = setInterval(drawCard, 250);};
 });
